532b611298a92a2008327da754923d1e791334c1
sj <zkdlwjsxm@nate.com>
2024-04-09 21:48:48 +0900

    feat: friend list,delete 추가

--- src/main/java/banzzac/mapper/FriendMapper.java
+++ src/main/java/banzzac/mapper/FriendMapper.java
@@ -8,8 +8,32 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import banzzac.dto.FriendDTO;
+
 @Mapper
 public interface FriendMapper {
+	//* 친구리스트*/
+	@Select("select * from friend where id=#{id} and block=1")
+	List<FriendDTO> list(String id);
+	//* 차단 친구 리스트 */
+	@Select("select * from friend where id=#{id}  and block=0")
+	List<FriendDTO> blockList(String id);
+	//* 즐겨찾기 친구 리스트*/
+	@Select("select * from friend where id=#{id}  and block=2")
+	List<FriendDTO> favoriteList(String id);
 	
+	//* 친구차단 */
+	@Update("update friend set block=0 where id=#{id} and friendId={friendId}") //차단,확인해야함
+	String friendBlock(FriendDTO dto);
+	//* 즐겨찾기친구추가 */
+	@Update("update friend set block=2 where id=#{id} and friendId={friendId}") //즐찾 ,확인해야함
+	String friendFavorite(FriendDTO dto);
+	
+	//*친구삭제*/
+	@Delete("delete from friend where id=#{id} and friend_id=#{friendId}")
+	int delete(FriendDTO dto);
+	
+	
+		
 }
 		
