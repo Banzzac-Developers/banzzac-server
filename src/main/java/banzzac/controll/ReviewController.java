07bc835418736166540b04953de985e3cd7b9c4c
HyeseongUm <5391nks@naver.com>
2024-04-09 16:53:20 +0900

    review 조회sql문 변경하기, 삭제는 가능

--- src/main/java/banzzac/controll/ReviewController.java
+++ src/main/java/banzzac/controll/ReviewController.java
@@ -1,16 +1,16 @@
 package banzzac.controll;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import banzzac.dto.ReviewDTO;
 import banzzac.mapper.ReviewMapper;
 import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("/api/review")
-@CrossOrigin("http://localhost:3000/")
 public class ReviewController {
 
 	@Resource
@@ -20,6 +20,30 @@ public class ReviewController {
 	Object list() {
 		return mapper.list();
 	}
+
+	@GetMapping("/{no}")
+	Object detail(ReviewDTO dto) {
+		return mapper.delete(dto);
+	}
 	
+	@PostMapping("/{no}/modify")
+	Object modify(ReviewDTO dto) {		
+		int res = mapper.modify(dto);
+		if(res!=0) {
+			return mapper.list();
+		}
+		return "수정 실패";
+	}
+	
+	
+	@GetMapping("/{no}/delete")
+	Object delete(ReviewDTO dto) {
+		System.out.println("들어옴 ? : "+ dto);
+		int res = mapper.delete(dto);
+		if (res >=1) {
+			return mapper.list();
+		}
+		return "삭제 실패";
+	}
 	
 }
--- src/main/java/banzzac/mapper/ReviewMapper.java
+++ src/main/java/banzzac/mapper/ReviewMapper.java
@@ -2,15 +2,35 @@ package banzzac.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import banzzac.dto.ReviewDTO;
 
 @Mapper
 public interface ReviewMapper {
 
-	/** 최근 리뷰리스트 -> where my_id : session id 와 동일한 것만 select */ 
+	/** 최근 리뷰리스트 -> where my_id : session id 의 list select 
+	 => 약속잡기의 my_id 와 your_id 가 같아야함
+	 */ 
 	@Select("select * from review where my_id='aaa' order by reg_date desc") 
 	List<ReviewDTO> list();
+	
+	/** 리뷰 수정 */
+	@Update("update review set review_score=#{reviewScore}, content=#{content} where no = #{no}")
+	int modify(ReviewDTO dto);
+	
+	/** 리뷰 삭제 */
+	@Delete("delete from review where no=#{no}")
+	int delete(ReviewDTO dto);
+	
+	/* 
+	 온도 계산 
+	 
+	 1. review , member table join / where your_id = id
+	 2. select reviewScore review table 
+	 3. reviewScore*20 / cnt +1 => member table Temperature / cnt 에 insert	  
+	 */
 }
