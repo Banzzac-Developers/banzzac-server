9321690ad26fc737234e280a9f3371fbd298bb88
Ryu_woong <jin02014@naver.com>
2024-04-13 05:15:05 +0900

    feat/Paging 페이징 기능 추가 상세 설명 AdminController에 작성

--- src/main/java/banzzac/utill/CommonLayout.java
+++ src/main/java/banzzac/utill/CommonLayout.java
@@ -1,13 +1,10 @@
 package banzzac.utill;
 
-import jakarta.annotation.Resource;
 import lombok.Data;
 
 @Data
 public class CommonLayout {
 	private String folder;
 	private String service;
-	@Resource
-	private Paging page; 
 	
 }
