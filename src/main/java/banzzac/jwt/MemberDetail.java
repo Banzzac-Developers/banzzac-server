4bc52aca5537f2e610a9af199aed8aeb0e3d6fb1
Ryu_woong <jin02014@naver.com>
2024-05-08 05:05:10 +0900

    feat: JWT Token 발급 완료, 검증 및 재발급 실험 전
    관리자 페이지로 전환 되도록 수정해야 함. JWT를 어떻게 PingPong 해야하는 지 아직 미구현, Front 연결 후에 실험하도록 하겠음

--- src/main/java/banzzac/jwt/MemberDetail.java
+++ src/main/java/banzzac/jwt/MemberDetail.java
@@ -1,78 +1,24 @@
 package banzzac.jwt;
 
 import java.util.Collection;
-
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.Data;
 
-import banzzac.dto.MemberDTO;
-import jakarta.annotation.Resource;
-
-/** Spring Security 에서 제공하는 Interface를 구현하여 인증에 관련된 메소드들을 구현합니다.*/
-public class MemberDetail implements UserDetails {
+@Data
+public class MemberDetail extends User{
 
 	private static final long serialVersionUID = 1L;
 	
-	@Resource
-	private MemberDTO memberDTO;
-	/** 계정의 권한 목록들 가져오는 메소드*/
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return this.getAuthorities();
+	private int no,isGrant;
+	private String id,pwd,img,nickname;
+	
+	public MemberDetail(String username,
+			String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username,password,authorities);
 	}
 	
-	
-	@Override
-	public String getPassword() {
-		return null;
-	}
-	
-	
-	/** JsonProperty read_write 권한,
-	 * 아이디를 반환.*/
-	@JsonProperty(access = Access.WRITE_ONLY)
-	@Override
-	public String getUsername() {
-		return memberDTO.getId();
-	}
-
-	
-	/** 계정이 만료되었는 지 가져오는 메소드 true 시 계정 만료X , False에 만료 O*/
-	@JsonProperty(access = Access.WRITE_ONLY)
-	@Override
-	public boolean isAccountNonExpired() {
-		return false;
-	}
-	
-	
-	/** 계정이 잠겼는지 확인하는 메소드 true 시 계정 만료 X, 위와 같음*/
-	@JsonProperty(access = Access.WRITE_ONLY)
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-	
-	
-	/** 계정이 비밀번호가 만료 되었는지 확인하는 메소드, 위와 같음.*/
-	@JsonProperty(access = Access.WRITE_ONLY)
-	@Override
-		// TODO Auto-generated method stub
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	
-	
-	/** 계정이 활성화 되었는지 확인하는 메소드, true가 활성화 상태.*/
-	@JsonProperty(access = Access.WRITE_ONLY)
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
 	
 }
