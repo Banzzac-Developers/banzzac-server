145e6a14071e3e2300d9b84250150d21c29d85cf
HyeseongUm <5391nks@naver.com>
2024-05-11 18:55:44 +0900

    feat:walkController jwt로 변경

--- src/main/java/banzzac/rest/WalkController.java
+++ src/main/java/banzzac/rest/WalkController.java
@@ -1,10 +1,10 @@
 package banzzac.rest;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import banzzac.dto.MemberDTO;
 import banzzac.dto.ReviewDTO;
 import banzzac.dto.WalkDiaryDTO;
+import banzzac.jwt.MemberDetail;
 import banzzac.mapper.ReviewMapper;
 import banzzac.utill.CommonResponse;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/walk")
@@ -26,34 +26,23 @@ public class WalkController {
 
 	@Resource
 	ReviewMapper mapper;
-	@GetMapping("session")
-	public ResponseEntity<CommonResponse<MemberDTO>> settingSession(HttpSession session, MemberDTO dto){
-		dto.setId("example3@example.com");
-		dto.setNo(3);
-		dto.setDate(new Date());
-		session.setAttribute("member", dto);
-		System.out.println("세션 설정" + dto + ", " + session.getAttribute("banzzacMember"));
-		
-		
-		return CommonResponse.success(dto);
-		
-	}
-	
-	
+
 	/**4. 산책 다이어리 리스트 가져오기 */
 	@GetMapping("diaries")
-	public ResponseEntity<CommonResponse<ArrayList<WalkDiaryDTO>>> getWalkingDiary(HttpSession session){
+	public ResponseEntity<CommonResponse<ArrayList<WalkDiaryDTO>>> getWalkingDiary(Authentication auth){
 		
-		MemberDTO info = (MemberDTO)session.getAttribute("banzzacMember");
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		MemberDTO info = new MemberDTO();
+		info.setId(myId.getId());
 		
 		return CommonResponse.success(mapper.getWalkList(info));
 	}
 	
 	/**1. 약속 잡기 추가*/
 	@PostMapping("promise")
-	public ResponseEntity<CommonResponse<WalkDiaryDTO>> insertPromise(@RequestBody WalkDiaryDTO dto,HttpSession session){
-		MemberDTO info = (MemberDTO)session.getAttribute("banzzacMember");
-		dto.setSessionId(info.getId());
+	public ResponseEntity<CommonResponse<WalkDiaryDTO>> insertPromise(@RequestBody WalkDiaryDTO dto,Authentication auth){
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		dto.setSessionId(myId.getId());
 		if(mapper.createPromise(dto) >= 1) {
 			return CommonResponse.success(mapper.getInsertPromise(dto));			
 		}
@@ -63,10 +52,10 @@ public class WalkController {
 	
 	/**2. 승낙 거절*/
 	@PostMapping("promise/{promiseStatus}")
-	public ResponseEntity<CommonResponse<String>> updatePromiseStatus(HttpSession session,@RequestBody WalkDiaryDTO dto ,@PathVariable Integer promiseStatus){
+	public ResponseEntity<CommonResponse<String>> updatePromiseStatus(Authentication auth,@RequestBody WalkDiaryDTO dto ,@PathVariable Integer promiseStatus){
 		
-		MemberDTO info =(MemberDTO)session.getAttribute("banzzacMember");
-		dto.setSessionId(info.getId());
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		dto.setSessionId(myId.getId());
 		
 		if(mapper.updatePromiseStatus(dto) == 0 ) {
 			return CommonResponse.error(HttpStatus.BAD_REQUEST, "error - 404.2", "요청에 실패했습니다.");
@@ -90,9 +79,11 @@ public class WalkController {
 	
 	/**5. 평가하기 */
 	@PostMapping("appraisal")
-	public ResponseEntity<CommonResponse<ArrayList<WalkDiaryDTO>>> appraiseWalker(@RequestBody ReviewDTO dto, HttpSession session){
+	public ResponseEntity<CommonResponse<ArrayList<WalkDiaryDTO>>> appraiseWalker(@RequestBody ReviewDTO dto, Authentication auth){
 		
-		MemberDTO info = (MemberDTO)session.getAttribute("banzzacMember");
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		MemberDTO info = new MemberDTO();
+		info.setId(myId.getId());
 		dto.setMyId(info.getId());
 		if(mapper.registerReview(dto) >= 1) {
 			return CommonResponse.success(mapper.getWalkList(info));			
@@ -105,10 +96,10 @@ public class WalkController {
 	
 	/**6. 리뷰 수정 후 내용만 수정*/
 	@PostMapping("review/{no}")
-	public ResponseEntity<CommonResponse<ReviewDTO>> modifyReview(@RequestBody ReviewDTO dto,HttpSession session,@PathVariable int no){
+	public ResponseEntity<CommonResponse<ReviewDTO>> modifyReview(@RequestBody ReviewDTO dto,Authentication auth,@PathVariable int no){
 		
-		MemberDTO info = (MemberDTO)session.getAttribute("banzzacMember");
-		dto.setMyId(info.getId());
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		dto.setMyId(myId.getId());
 		dto.setNo(no);
 		
 		if(mapper.modify(dto) >= 1) {
@@ -123,10 +114,10 @@ public class WalkController {
 	
 	/**7. 리뷰 삭제 */
 	@GetMapping("review/{no}/delete")
-	public ResponseEntity<CommonResponse<Integer>> deleteReview(HttpSession session, @PathVariable int no){
+	public ResponseEntity<CommonResponse<Integer>> deleteReview(Authentication auth, @PathVariable int no){
 		ReviewDTO dto = new ReviewDTO();
-		MemberDTO info = (MemberDTO)session.getAttribute("banzzacMember");
-		dto.setMyId(info.getId());
+		MemberDetail myId = (MemberDetail)auth.getPrincipal();
+		dto.setMyId(myId.getId());
 		dto.setNo(no);
 		System.out.println("delete Review dto : " + dto);
 		int res = mapper.delete(dto);
